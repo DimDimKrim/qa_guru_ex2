b8a1dea4ddf6d5fe14ad003f44c4887ab6d6e9a4
Dmitrii V <dvsvpl92@gmail.com>
2025-07-16 20:01:42 +0300

    Добавил Allure отчёты

--- test_main.py
+++ test_main.py
@@ -1,5 +1,8 @@
 from selene import browser, be, have
+import allure
 
+@allure.feature('Некорректный поиск')
+@allure.story('Проверка некорректного ввода данных')
 def test_no_match():
     browser.element('[name="q"]').should(be.visible).type("adsdasвышгрфыш!№;%га561253!").press_enter()
     browser.element('[class="gSXOPxXJQAIpv4HDaDFd"]').should(have.text('По запросу «adsdasвышгрфыш!№;%га561253!» ничего не найдено.'))
